3d382df8270bd2b2cda9e55b1e2a13fe56636803
Martin Bridgman <martin.bridgman@gmail.com>
2022-02-07 01:29:12 +0000

    API throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -68,7 +68,11 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else {
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()} before making another request.</h4>`)
+            }
+            else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
